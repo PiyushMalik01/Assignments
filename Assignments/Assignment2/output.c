69a0260bdb8799eb4f9464830c9c6df7c7c17352
PIYUSH MALIK <147936084+PiyushMalik01@users.noreply.github.com>
2024-01-25 09:37:57 +0530

    Add files via upload

---
+++ Assignments/Assignment2/output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    // feeds a new line
+    printf("Hello\n");
+    // moves cursor one space back
+    printf("Hello World Hello World h\b");
+    // give a couple of space to the right
+    printf("Hello World Hello World\t");
+    printf("Hello");
+    return 0;
+}
